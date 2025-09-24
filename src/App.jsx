d9f61ed318a180d5555784d6c34a7f9d9e38630c
19Lida <lida19melnyk@gmail.com>
2025-09-24 20:39:23 +0200

    add tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,87 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // 2
+ const [tasks,setTasks]= useState([]);
+ const [inputValue,setInputValue]= useState('')
+
+//  3
+ const addTask=()=>{
+  if(inputValue.trim()==='') return;
+
+  const newTask={
+    id:Date.now(),
+    text:inputValue,
+    completed:false,
+    
+  }
+  
+  setTasks([...tasks,newTask])
+  setInputValue('')
+  console.log(newTask)
+ }
+
+ // 5
+   const deleteTask=(id)=>{
+     console.log("Видаляємо задачу з id:", id);
+
+    const updatedTasks=tasks.filter((task)=>task.id !==id)
+    console.log("Старий масив:", tasks);
+  console.log("Новий масив після filter:", updatedTasks);
+  setTasks(updatedTasks);
+   
+  }
+    const toggleTask = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+   
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    // 1
+  <div className='app'>
+    <h1 className='title'>My To-Do App</h1>
+    <div className="input-wrapper">
+      <input 
+      type="text"
+      // 4
+      value={inputValue}
+      onChange={(e)=>setInputValue(e.target.value)}
+      // 
+      placeholder='Введіть задачу...'
+      className='task-input'
+      />
+      <button onClick={addTask} className='add-btn'>
+         Додати
+      </button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      {/* 5 */}
+      <ul className="task-list">
+        {tasks.map((task)=>(
+          <li className='task-list-item'>
+            <div className="task-left">
+            <input type="checkbox"
+            checked={task.completed}
+            onChange={()=>toggleTask(task.id)}
+             className="task-checkbox"
+              />
+            <span className={`task-text ${task.completed ? "completed" : ""}`}>
+              {task.text}
+              </span>
+</div>
+          {/* 5 */}
+        <button className="delete-btn" onClick={()=>deleteTask(task.id)} >
+            ✖  Видалити
+            </button>
+          </li>
+        ))}
+      </ul>
+  </div>)
 }
 
 export default App
